348783842f9ebbdf57cfcfe0d3d1884bba87310a
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-28 08:54:18 +0100

    piccola modifica dei link delle imamgini play e stop

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ playButton.addEventListener('click', changeImage);
 function changeImage() {
     const sourceImage = playButton.src;
     console.log(sourceImage);
-    if(sourceImage === 'http://127.0.0.1:5500/images/assets_play.svg') {
-        playButton.src = './images/assets_stop-2.svg';
-    } else if(sourceImage === 'http://127.0.0.1:5500/images/assets_stop-2.svg') {
-        playButton.src = './images/assets_play.svg';
+    // https://matteor1994.github.io/copyJazzFMRomania/images/assets_play.svg
+    // https://matteor1994.github.io/copyJazzFMRomania/images/assets_stop-2.svg
+    if(sourceImage === 'https://matteor1994.github.io/copyJazzFMRomania/images/assets_play.svg') {
+        playButton.src = 'https://matteor1994.github.io/copyJazzFMRomania/images/assets_stop-2.svg';
+    } else if(sourceImage === 'https://matteor1994.github.io/copyJazzFMRomania/images/assets_stop-2.svg') {
+        playButton.src = 'https://matteor1994.github.io/copyJazzFMRomania/images/assets_play.svg';
     }
 }
